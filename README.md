6d1f6cdfcfcc6f6c8654bdd905f33976b665c5ef
kikimanjaro <kylian.meulin@gmail.com>
2023-01-20 23:43:08 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pooltogether App
+
+A Flutter app for PoolTogether, made using a WebView.
---
+++ android/app/src/main/kotlin/com/example/pooltogetherapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pooltogetherapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
